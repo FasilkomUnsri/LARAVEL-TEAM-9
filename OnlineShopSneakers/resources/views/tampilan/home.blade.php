6fa734667afdc7f7e5932916883375263e44052d
RajabAbdillah <90497515+RajabAbdillah@users.noreply.github.com>
2021-11-23 22:19:48 +0700

    create home

---
+++ OnlineShopSneakers/resources/views/tampilan/home.blade.php
@@ -0,0 +1,276 @@
+{{-- halaman ini mengambil dari rio/main --}}
+@extends('rio/main')
+@section('main')
+    <!------------ featured categoty -------------->
+    <div class="categories">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <a href="product detail trafiscott.html"><img src="img/cactus1.jpg" ></a>
+                </div>
+                <div class="col-3">
+                    <a href="produk air dior .html"><img src="img/retro1.jpg" ></a>
+                </div>
+                <div class="col-3">
+                    <a href="aif force.html"><img src="img/levi1.jpg" ></a>
+                </div>
+        </div>
+        </div>
+       
+    </div>
+    <!------------------featured product------------------>
+
+    <div class="small-container">
+        <h2 class="title">Featured Products</h2>
+        <div class="row">
+            <div class="col-4">
+                <a href="Product-detail.html"><img src="img/cactus1.jpg"></a>
+               <a href="Product-detail.html"><h4>AIR FORCE 1 LOW "Off-White - MCA"</h4></a> 
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>24,089,000</P>
+            </div>
+            <div class="col-4">
+                <a href="product detailnikeairzoom tera.html"><img src="img/cactus1.jpg" ></a>
+                <a href="product detailnikeairzoom tera.html"><h4>THE 10: NIKE AIR FORCE 1 LOW "Off-White Volt"</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>Rp 31,799,000</P>
+            </div>
+            <div class="col-4">
+                <a href="product detail trafiscott.html"><img src="img/cactus1.jpg" ></a>
+                <a href="product detail trafiscott.html"><h4>FORCE 1 PS "Cactus Jack - Travis Scott"</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>Rp21.389.000</P>
+            </div>
+            <div class="col-4">
+                <a href="product detail nike 107.html"><img src="img/cactus1.jpg" ></a>
+                <a href="product detail nike 107.html"><h4>X SPACE JAM AIR FORCE 1 SNEAKERS</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>Rp 17,979,000</P>
+            </div>
+        </div>
+        <h2 class="title">Latest Products</h2>
+        <div class="row">
+            <div class="col-4">
+                <a href="produk air dior .html"><img src="img/gold1.jpg" ></a>
+                <a href="produk air dior .html"><h4>NIKE AIR JORDAN DIOR HIGH RETRO</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>Rp 190.999.999</P>
+            </div>
+            <div class="col-4">
+                <a href="product kayrie.html"><img src="img/gold1.jpg" ></a>
+                <a href="product kayrie.html"><h4> AIR FORCE 1 LOW SUPREME "Krink" </h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>Rp19.979.000</P>
+            </div>
+            <div class="col-4">
+                <a href="aif force.html"><img src="img/gold1.jpg" ></a>
+                <a href="aif force.html"><h4>AIR FORCE 1 X "Levi's Denim" LOW</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>Rp 41,499,000</P>
+            </div>
+            <div class="col-4">
+                <a href="sean.html"><img src="img/gold1.jpg" ></a>
+                <a href="sean.html"><h4>AIR MAX 1/97 VF SW "Sean Wotherspoon"</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>RP 25.599.000</P>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <a href="jam1.html"><img src="img/gold1.jpg" ></a>
+                <a href="jam1.html"><h4>AIR FORCE 1 LOW "Off-White - University Gold"</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    
+                </div>
+                <P>RP 44.599.000</P>
+            </div>
+            <div class="col-4">
+                <a href="jam4.html"><img src="img/gold1.jpg" ></a>
+                <a href="jam4.html"><h4>SB DUNK LOW "Supreme - Stars - Hyper Blue"</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                    
+                </div>
+                <P>RP 33.599.000</P>
+            </div>
+            <div class="col-4">
+                <a href="jam2.html"><img src="img/gold1.jpg" ></a>
+                <a href="jam2.html"><h4>NIKE 1 WMNS AIR FORCE 1 LOW"White / Grey / Gold"</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                    
+                </div>
+                <P>RP 13.599.000</P>
+            </div>
+            <div class="col-4">
+                <a href="MC1.html"><img src="img/gold1.jpg" ></a>
+                    <a href="MC1.html"><h4>NIKE AIR FORCE 1 LOW STRIPES SNEAKERS</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                    
+                </div>
+                <P>RP 5.599.000</P>
+            </div>
+    </div>
+<!------------offer----------------->
+    <div class="offer">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-2">
+                    <img src="img/dior1.jpg" class="offer-img">
+                </div>
+                <div class="col-2">
+                <h4>Exclusive Available On DTXSNEAKERS</h4>
+                    <h1>Air Jordan 1 Dior </h1>
+                    <small>For the Men’s Fall 2020 Runway show in Miami, Dior and Kim Jones partnered with Jordan Brand to unveil the limited-edition Air Jordan 1 High OG Dior sneaker. A collection of ready-to-wear and accessories completes the collaboration, and a low version of the Air Jordan 1 OG Dior rounds out the range.</small>
+                  <br>  <a href="/NikeAirJordanDior" class="btn">Buy Now &#8594;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<!---------------testimonial---------------->
+<div class="testimonial">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>Keren banget sih sepatunya udah 2 kali belaja di sini gambarnya sesuai...aku suka banget belanja disini</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <a href="https://www.instagram.com/rioalfido/"><img src="img/ji.jpg" ></a>
+                <h3> Otong</h3>
+            </div>
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>ihh aku suka banet sama sepatunya bagus bagus warnanya dan bentuknya aku suka senang sekali belanja disini </p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <a href="https://www.instagram.com/rioalfido/"><img src="img/ji.jpg" ></a>
+                <h3>Sukri</h3>
+            </div>
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>여기에서 한 번 밖에 쇼핑하지 않았지만 결과는 좋습니다 신발이 마음에 듭니다 아마도 이것이 내 단골 가게가 될 것입니다</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <a href="https://www.instagram.com/sooyaaa__/"><img src="img/ji.jpg"></a>
+                <h3> SOYAAA </h3>
+            </div>
+        </div>
+    </div>
+</div>
+            </div>
+        </div>
+    </div>
+<!--------------brands-------------->
+<div class="brands">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-5">
+                <img src="img/bca1.png" >
+            </div>
+            <div class="col-5">
+                <img src="img/dana1.png" >
+            </div>
+            <div class="col-5">
+                <img src="img/mm.png" >
+            </div>
+            <div class="col-5">
+                <img src="img/gopay-logo.png" >
+            </div>
+            <div class="col-5">
+                <img src="img/logo-paypal.png" >
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
